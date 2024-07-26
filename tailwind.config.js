e3035f16abc1eeea51af56eadec71c5e127d23ea
webNekit <mochalov-hunter@mail.ru>
2024-07-26 14:11:51 +0300

    start build header

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        primary: "Ubuntu, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+}
